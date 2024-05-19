37c5c6a843d9dc334f3b273595687c91d854bb67
Bao Khoa <khoatptk1@gmail.com>
2024-05-19 10:37:13 +0700

    feat: invitation route

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -18,20 +18,28 @@ const createNew = async (reqBody) => {
     return getNewCard
   } catch (error) { throw error }
 }
-const update = async (cardId, reqBody, cardCoverFile) => {
+const update = async (cardId, reqBody, cardCoverFile, userInfo) => {
   try {
     const updateData = {
       ...reqBody,
-      updatedAt: Date.now(),
+      updatedAt: Date.now()
     }
     let updatedCard = {}
 
     if (cardCoverFile) {
       // Trường hợp đẩy ảnh lên Cloudinary
       const uploadResult = await CloudinaryProvider.streamUpload(cardCoverFile.buffer, 'card-covers')
-
       // Lưu lại url của cái file ảnh vào trong database
       updatedCard = await cardModel.update(cardId, { cover: uploadResult.secure_url })
+    } else if (updateData.commentToAdd) {
+      // Tạo dữ liệu comment để thêm vào Database, cần bổ sung thêm những field cần thiết
+      const commentData = {
+        ...updateData.commentToAdd,
+        commentedAt: Date.now(),
+        userId: userInfo._id,
+        userEmail: userInfo.email
+      }
+      updatedCard = await cardModel.unshiftNewComment(cardId, commentData)
     } else {
       // Các trường hợp update chung như title, description, ...
       updatedCard = await cardModel.update(cardId, updateData)
